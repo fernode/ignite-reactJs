b71445c25b3cfa777804639ec27ab06a3d3acd39
Fernando Silva <fernandosilva@MacBook-Air-de-Fernando.local>
2021-08-24 22:40:11 -0300

    feat - finish aplication

---
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import RepositoryItem from './RepositoryItem'
+import '../styles/repositories.scss'
+
+interface Repository {
+  name: string
+  description: string
+  html_url: string
+}
+
+function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/danielobara/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+  }, [repositories])
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+      <ul>
+        {repositories.map((repositorie) => (
+          <RepositoryItem key={repositorie.name} repository={repositorie} />
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default RepositoryList
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -7,26 +7,28 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    hot: true,
-    compress: true,
-    port: 9000
-  },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    hot: true
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ].filter(Boolean),
   module: {
     rules: [
       {
-        test: /\.[jt]sx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -42,12 +44,6 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader']
       }
-    ].filter(Boolean)
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-    isDevelopment && new ReactRefreshWebpackPlugin()
-  ].filter(Boolean)
+    ]
+  }
 }
