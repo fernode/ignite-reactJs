f6abf74929f1ad38139bfa6310265099b7384e7a
Fernando Silva <fernandosilva@MacBook-Air-de-Fernando.local>
2021-09-14 23:56:59 -0300

    feat - add stripe api

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface SubscriptionButtonProps {
+	priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscriptionButtonProps) {
 	return <button className={styles.subscribeButton}>Subscribe now</button>
 }
