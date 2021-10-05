d9e691383fb2ebdb21d27422ca7d3db78ca412b7
Fernando Silva <fernandosilvadj@hotmail.com>
2021-10-04 23:24:49 -0300

    feat: create posts page
    create posts page to avoid not found
    - this page will be needed in future

---
+++ 03-ignews/src/pages/posts.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Posts({}) {
+	return (
+		<>
+			<Head>
+				<title>Posts | Ig news</title>
+			</Head>
+
+			<main>
+				<h1>Aqui ficaram os posts</h1>
+			</main>
+		</>
+	)
+}
