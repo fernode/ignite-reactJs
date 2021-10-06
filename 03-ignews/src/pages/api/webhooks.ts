3c9a709483d9e6a4efb482bb2855e4bed64281ed
Fernando Silva <fernandosilvadj@hotmail.com>
2021-10-05 22:48:10 -0300

    feat: listen stripe events
    get events that application return
    - get what user does

--- 03-ignews/src/pages/api/webhooks.ts
+++ 03-ignews/src/pages/api/webhooks.ts
@@ -1,8 +1,52 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from 'services/stripe'
 import { Readable } from 'stream'
+import Stripe from 'stripe'
 
-export default function hooks(req: NextApiRequest, res: NextApiResponse) {
-	console.log('evento recebido')
+async function buffer(readable: Readable) {
+	const chunks = []
 
-	return res.status(200).json({ ok: true })
+	for await (const chunk of readable) {
+		chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+	}
+
+	return Buffer.concat(chunks)
+}
+
+export const config = {
+	api: {
+		bodyParser: false
+	}
+}
+
+const relevantEvents = new Set('checkout.session.completed')
+
+export default async function hooks(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		const buf = await buffer(req)
+		const secret = req.headers['stripe-signature']
+
+		let event: Stripe.Event
+
+		try {
+			event = stripe.webhooks.constructEvent(
+				buf,
+				secret,
+				process.env.STRIPE_WEBHOOK_SECRET
+			)
+		} catch (error) {
+			return res.status(404).send(`Webhook error ${error.message}`)
+		}
+
+		let type = event.type
+
+		if (relevantEvents.has(type)) {
+			console.log(`Evento recebido ${event}`)
+		}
+
+		return res.json({ received: true })
+	} else {
+		res.setHeader('Allow', 'POST')
+		res.status(405).end('Method is not allowed')
+	}
 }
