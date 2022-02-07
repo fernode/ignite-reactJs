52bd5a06459fab0d06024106dd2347b4b6eee332
Fernando Silva <fernandosilvadj@hotmail.com>
2022-02-07 16:21:07 -0300

    feat: add post page

---
+++ 03-ignews/src/pages/posts/[slug].tsx
@@ -0,0 +1,72 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import React from 'react'
+import Client from 'services/prismic'
+import Prismic from '@prismicio/client'
+import { getSession } from 'next-auth/client'
+import { RichText } from 'prismic-dom'
+import styles from './post.module.scss'
+
+interface PostProps {
+	post: {
+		slug: string
+		title: string
+		content: string
+		updatedAt: string
+	}
+}
+
+function Post({ post }: PostProps) {
+	const router = useRouter()
+	if (router.isFallback) return null
+
+	return (
+		<>
+			<Head>
+				<title>{post.title}</title>
+			</Head>
+			<main className={styles.container}>
+				<article className={styles.post}>
+					<h1>{post.title}</h1>
+					<time>{post.updatedAt}</time>
+					<div
+						className={styles.postContent}
+						dangerouslySetInnerHTML={{ __html: post.content }}
+					/>
+				</article>
+			</main>
+		</>
+	)
+}
+
+export const getServerSideProps = async ({ req, params }) => {
+	const session = await getSession({ req })
+	const { slug } = params
+
+	const prismic = Client(req)
+	const response = await prismic.getByUID('publication', String(slug), {})
+
+	if (!response) return { notFound: true }
+
+	const post = {
+		slug,
+		title: RichText.asText(response.data.title),
+		content: RichText.asHtml(response.data.content),
+		updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+			'pt-BR',
+			{
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			}
+		)
+	}
+
+	return {
+		props: {
+			post
+		}
+	}
+}
+
+export default Post
